2b3f53209fb62fa70b058f7c22f54d22dd2f40f4
Maksims Carevs <mca@vtl.lv>
2025-09-23 13:40:37 +0300

    ssh key test"

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,7 +14,8 @@ class AuthController extends Controller
             'password' => 'required|confirmed'
         ]);
 
-        $user = User::create
+        $user = User::create($fields);
+        return $user;
     }
     public function login(Request $request){
         return 'login';
